afc4d200f405b2262a34766ec0945938cc50de2b
CoderDam <garnes.damien@gmail.com>
2017-02-10 18:53:40 +0100

    LINK.js ajout position initiale Link + les 4 types de déplacement

--- js/link.js
+++ js/link.js
@@ -1,4 +1,9 @@
 var link = {
+	/**
+	*	Variables
+	*/
+	posX: (2*16-2.5),
+	posY: (map.tiles.length*16-22),
   /*
    * 1. Créer un élément DOM
    * 2. Le positioner à map.startPosition
@@ -8,12 +13,21 @@ var link = {
     link.dom = document.createElement('div');
     link.dom.id = 'link';
     link.dom.className = 'front';
-
-
-
+		// on fixe la position initiale
+		link.dom.style.top = link.posY+'px';
+		link.dom.style.left = link.posX+'px';
 
     return link.dom;
   },
+
+
+	/*
+	* Déclenche les fonctions moveTop, moveRight, moveBottom, moveLeft
+	* en fonction de la touche qui a été appuyée
+	*/
+	moveHandler: function(event) {
+
+	},
 
 
   /*
@@ -23,7 +37,9 @@ var link = {
    * https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Instructions/switch
    */
   move: function(posX, posY) {
+		if (isMovementAllowded(posX) || isMovementAllowded(posY)) {
 
+		}
   },
 
 
@@ -32,28 +48,27 @@ var link = {
    * Utilise move
    */
   moveLeft: function() {
-
+		link.dom.className = 'left';
+		link.posX -= 16;
+		link.dom.style.left = link.posX+'px';
   },
 
   moveRight: function() {
-
+		link.dom.className = 'right';
+		link.posX += 16;
+		link.dom.style.left = link.posX+'px';
   },
 
   moveTop: function() {
-
+		link.dom.className = 'back';
+		link.posY -= 16;
+		link.dom.style.top = link.posY+'px';
   },
 
   moveBottom: function() {
-
-  },
-
-
-  /*
-   * Déclenche les fonctions moveTop, moveRight, moveBottom, moveLeft
-   * en fonction de la touche qui a été appuyée
-   */
-  moveHandler: function(event) {
-
+		link.dom.className = 'front';
+		link.posY += 16;
+		link.dom.style.top = link.posY+'px';
   },
 
 
