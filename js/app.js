67773ffee5760dd9c266dece5434be7f73278b46
CoderDam <garnes.damien@gmail.com>
2017-02-10 22:10:39 +0100

    APP.js dynamisation création map

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@ var app = {
    * 2. L'élément #map doit aussi recevoir en style des dimensions, en fonction de la longueur de l'objet map (de js/map.js). Chaque tuile est un carré 16 pixels.
    */
   init: function() {
-    //On fait appel à la fonction
+    // on crée #map
+		app.createMap();
+    // on crée les tuiles
     app.createTiles();
-		// on récupère #map
-		app.mapDOM = document.getElementById('map');
     // on crée le personnage
     app.mapDOM.appendChild(link.create());
 		// on écoute les touches de mvt
@@ -20,19 +20,28 @@ var app = {
    * 3. Dans #map, on va venir ajouter en enfant toutes les tuiles .tile.
    */
   createTiles: function() {
-
     //Parcours de la liste des tuiles pour récupérer chaque tuile.
     for (var line = 0; line < map.tiles.length; line++) {
-
       for (var column = 0; column < map.tiles[line].length; column++) {
         // Préparation de la création de la tuile.
-
-
         tile.create(column, line);
       }
     }
-
   },
+
+	createMap: function() {
+		// on crée #map
+		app.mapDOM = document.createElement('div');
+		// on lui ajoute l'id
+		app.mapDOM.id = 'map';
+		// on la style
+		app.mapDOM.style.height = map.tiles.length*16+'px';
+		app.mapDOM.style.width = map.tiles[0].length*16+'px';
+		// on l'envoie dans #container
+		var container = document.getElementById('container');
+		container.appendChild(app.mapDOM);
+	},
+
 }
 
 
